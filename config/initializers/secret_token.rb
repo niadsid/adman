a46786faf036e30fdf5fff491434a1f2f6a1f645
David Westgate <david.westgate@run-corp.com>
2011-06-25 20:17:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adman::Application.config.secret_token = 'bfea9e2fcbf79aacfe4fd5f76cb9bc792476292d4e06f0d2b2be666f93a2579f4472f92291c053a47e354e55aace0ce4eac5830c1a723e9160672d1934fe9686'
